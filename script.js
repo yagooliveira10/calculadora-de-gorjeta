94154b0a17e189be4f13d755f4bee26dc7121787
yagooliveira10 <yago.oliveira10@eaportal.org>
2026-05-23 06:50:50 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let conta = 0
 let pessoas = 0
+let porcentagem
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
@@ -25,3 +26,21 @@ function receberQuantidadePessoas(evento) {
       pessoas = Number(evento.target.value)
     }
 }    
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='buttonn']")
+    botoesGorjeta.forEach(botao => {
+        botao.addEventListener("click", receberPorcentagemBotao)
+    })
+
+function receberPorcentagemBotao(evento) {
+
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value){
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100
+    }
