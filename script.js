6ba3535256a10d1870e2763c7e2fbad123f4979b
yagooliveira10 <yago.oliveira10@eaportal.org>
2026-05-23 07:58:51 -0300

    quinto commit do JS

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ contaInput.addEventListener("input", receberValorConta)
 
 function receberValorConta(evento){
     conta = Number(evento.target.value) 
+    calcular()
 }
 
 const pessoasInput = document.querySelector("#pessoas")
@@ -25,6 +26,7 @@ function receberQuantidadePessoas(evento) {
       divErro.removeAttribute("id", "")
       pessoas = Number(evento.target.value)
     }
+    calcular()
 }    
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='buttonn']")
@@ -49,7 +51,37 @@ function receberPorcentagem(evento) {
     else{
         porcentagem = 0
     }
+
+    calcular
 }
 
     const gorjetaInput = document.querySelector("#outra")
-    gorjetaInput.addEventListener("input", receberPorcentagemBotao)
+    gorjetaInput.addEventListener("input", receberPorcentagem)
+
+    function calcular(){
+        if(conta !== 0 && porcentagem !==0 && pessoas !== 0){
+            const strongGorjetaTotal = document.querySelector(".gorjeta-total > strong")
+            strongGorjetaTotal.innerHTML = `R$ ${(conta * porcentagem / pessoas).toFixed(2)}`
+
+            const strongTotal = document.querySelector(".total > strong")
+        strongTotal.innerHTML = `R$ ${(((conta + (conta * porcentagem)) / pessoas)).toFixed(2)}`
+        }
+    }
+
+    const botaoLimpar = document.querySelector(".resultados button")
+    botaoLimpar.addEventListener("click", limpar)
+
+    function limpar(){
+        contaInput.value = ""
+
+        botoesGorjeta.forEach(botao => {
+            botao.classList.remove("botao-ativo")
+        })
+
+        gorjetaInput.value = ""
+
+        pessoasInput.value = ""
+
+        document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
+        document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+    }
